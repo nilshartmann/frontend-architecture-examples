c0d1ffa0d2060d1a05c9d0e1ca4b88ff8ea0a985
Nils Hartmann <nils@nilshartmann.net>
2023-09-01 12:31:21 +0200

    init

---
+++ src/main/java/nh/example/webappdemo/WebappDemoApplication.java
@@ -0,0 +1,13 @@
+package nh.example.webappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nh/example/webappdemo/WebappDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nh.example.webappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
